37f92a41a356d552837cacf2f13c0df653bb95c5
carter-willey <cjwilley23@gmail.com>
2021-06-22 10:52:30 -0500

    added initial files and placeholder classes based off uml

---
+++ userinterface.py
@@ -0,0 +1,32 @@
+class UserInterface:
+    @staticmethod
+    def display_message(message):
+        pass
+
+    @staticmethod
+    def get_user_input_string(prompt):
+        pass
+
+    @staticmethod
+    def get_user_input_int(prompt):
+        pass
+
+    @staticmethod
+    def display_contestant_info(contestant):
+        pass
+
+    @staticmethod
+    def display_sweepstakes_info(sweepstakes):
+        pass
+
+    @staticmethod
+    def display_sweepstakes_selection_menu(all_sweepstakes):
+        pass
+
+    @staticmethod
+    def display_marketing_firm_menu_options(marketing_firm_name):
+        pass
+
+    @staticmethod
+    def display_sweepstakes_menu_options(sweepstakes):
+        pass
